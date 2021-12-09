54057a6adcaac522ceb0151d41bc7c77470aedc9
programmer <info2016frei@gmail.com>
2021-12-09 05:20:33 -0500

    add weather history

---
+++ weather_history.py
@@ -0,0 +1,48 @@
+# https://meteostat.net/en/place/3WV0MB?t=2013-01-01/2013-12-31 source of data
+import pandas as pd
+import datetime
+from  datetime import date
+import calendar
+import os
+import numpy as np
+
+
+
+
+class Weather_history():
+    def __init__(self, path="/home/programmer/Downloads", place="ettenheim"):
+        self.path = path
+        self.place = place
+        self.all_data = []
+        self.years = ["2016","2017","2018","2019","2020"]
+        self.data_names = ["tsun", "snow", "tavg", "tmin", "tmax"]
+        starting_day_of_current_year = datetime.datetime.now().date().replace(month=1, day=1)
+        self.list_of_days = [starting_day_of_current_year + datetime.timedelta(days=i) for i in range(365)]
+        self.map_day_to_number = {(day.day, day.month):idx for idx, day in enumerate(self.list_of_days)}
+        self.all_sun_data = {i:[] for i in range(366)}
+
+    def init_data(self):
+        for year in self.years:
+            self.all_data.append(pd.read_csv(os.path.join(self.path, self.place + "_{}.csv".format(year))))
+        assert len(self.all_data) == len(self.years) 
+        #print(self.all_data[0])
+        self.data_of_all = []
+        for y in range(len(self.years)):
+            print(y)
+            for tyP in self.data_names:
+
+            for idx, day in enumerate(self.all_data[y]["tsun"]):
+                #print(day)
+                self.all_sun_data[idx].append(day)
+
+    def get_history(self, day, typ="sun"):
+        return self.all_sun_data[self.map_day_to_number[(day.day, day.month)]]
+
+
+
+
+if __name__ == "__main__":
+    today = date.today()
+    wh = Weather_history()
+    wh.init_data()
+    print(wh.get_history(today))
