aa5c68a29302cf46fb3242d72d7074d433ec86c5
Programmer <info2016frei@gmail.com>
2021-12-19 08:03:45 +0100

    add up to 15 days

---
+++ create_weather_history.py
@@ -0,0 +1,44 @@
+import datetime
+from  datetime import date
+import os
+import sys
+import pandas as pd
+
+from data_weather import Weather_data
+
+
+
+
+def main():
+    place= "Ettenheim"
+    year = today.year
+    month = today.month
+    day = today.day
+    path = "weather_data/{}/{}/{}/{}.txt".format(place, year, month, day)
+    directory = "weather_data/{}/{}/{}".format(place, year, month)
+    try:
+        os.makedirs(directory, exist_ok = True)
+        print("Directory '%s' created successfully" % directory)
+    except OSError as error:
+        print(error)
+    if os.path.exists(path):
+        print("data already there")
+        sys.exit()
+    today = date.today()
+    day = today.strftime("%Y-%m-%d")
+    days = 16
+    days_list = [(datetime.datetime.today() + datetime.timedelta(days=i)).strftime("%Y-%m-%d") for i in range(days)]
+    sol_dict = {}
+    for key in ws.keywords:
+        sol_dict.update({key:[]})
+
+    for idx in range(len(days_list)-1):
+        day = days_list[idx]
+        next_day = days_list[idx+1]
+        ws.get_data(day, next_day)
+        for key in ws.sol:
+            sol_dict[key].append(ws.sol[key])
+
+    data = pd.DataFrame(sol_dict)
+    data.set_axis(days_list[:-1])
+    data.to_csv(path)
